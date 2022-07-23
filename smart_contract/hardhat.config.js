4a1876c7c6e8f4d86eb0c2c75e1875d4edb175aa
bi305 <razab0715@gmail.com>
2022-07-23 13:03:14 +0500

    all  ok

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,3 +1,14 @@
 // https://eth-rinkeby.alchemyapi.io/v2/f3lwcQ6RaoXXcVSZzr-eMVFYX7Gwrrtu
 require("@nomiclabs/hardhat-waffle");
 
+module.exports = {
+	solidity: "0.8.0",
+	networks: {
+		rinkeby: {
+			url: "https://eth-rinkeby.alchemyapi.io/v2/f3lwcQ6RaoXXcVSZzr-eMVFYX7Gwrrtu",
+			accounts: [
+				"27c3c2223d80fd08d229ba378bf537c2c9923a8461f694886f33951293e7b3a4",
+			],
+		},
+	},
+};
--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,26 +1,22 @@
-
 const hre = require("hardhat");
 
 const main = async () => {
+	// We get the contract to deploy
+	const Transactions = await hre.ethers.getContractFactory("Transactions");
+	const transactions = await Transactions.deploy();
 
+	await greeter.deployed();
 
-  // We get the contract to deploy
-  const Transactions = await hre.ethers.getContractFactory("Transactions");
-  const transactions = await Transactions.deploy();
+	console.log("Transactions deployed to:", transactions.address);
+};
 
-  await greeter.deployed();
-
-  console.log("Transactions deployed to:", transactions.address);
-}
-
-
-const runMain = () => {
-  try {
-    await main()
-    process.exit(0)
-  } catch (error) {
-    console.error(error)
-    process.exit(1)
-  }
-}
-runMain()
+const runMain = async () => {
+	try {
+		await main();
+		process.exit(0);
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
+};
+runMain();
